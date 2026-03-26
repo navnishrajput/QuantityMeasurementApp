09d96b468eb42b2d32d368ef3aa09b94be227344
navnishrajput <navneeshkumarrajput@gmail,com>
2026-03-26 13:50:13 +0530

    Add Feet measurement equality logic

--- src/main/java/QuantityMeasurementApp/MeasurementApplication.java
+++ src/main/java/QuantityMeasurementApp/MeasurementApplication.java
@@ -6,8 +6,31 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MeasurementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MeasurementApplication.class, args);
-	}
+    static class Feet{
+        private final double value;
+        public Feet(double value){
+            this.value = value;
+        }
+        public boolean equals(Object obj){
+            if(this == obj) return  true;
+            if(obj == null || getClass() != obj.getClass()){
+                return  false;
+            }
+            Feet other = (Feet)obj;
+            return Double . compare(this.value, other.value) ==0;
+        }
+    }
+
+    public static void main(String[] args) {
+
+
+        SpringApplication.run(MeasurementApplication.class, args);
+
+        Feet f1 = new Feet(34.5);
+        Feet f2 = new Feet(34.5);
+        System.out.print("Answer: ");
+        System.out.println(f1.equals(f2));
+    }
+
 
 }
