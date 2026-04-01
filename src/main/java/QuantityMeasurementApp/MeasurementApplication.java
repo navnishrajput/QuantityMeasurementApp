6317cc14ddcae85df4f6ee54d8aa6fefbeba3f75
navnishrajput <navneeshkumarrajput@gmail,com>
2026-04-01 10:16:09 +0530

    Add generic length measurement logic for UC3

--- src/main/java/QuantityMeasurementApp/MeasurementApplication.java
+++ src/main/java/QuantityMeasurementApp/MeasurementApplication.java
@@ -17,15 +17,11 @@ public class MeasurementApplication {
             double value1 = 1.0;
             LengthUnit unit1 = LengthUnit.FEET;
 
-            double value2 = 12.0;
+            double value3 = 12.0;
             LengthUnit unit2 = LengthUnit.INCH;
 
             QuantityLength quantityLength = new QuantityLength(value1,unit1);
-            QuantityLength quantityLength1 = new QuantityLength(value2, unit2);
-
-
-
-
+            QuantityLength quantityLength1 = new QuantityLength(value3, unit2);
 
     }
 
